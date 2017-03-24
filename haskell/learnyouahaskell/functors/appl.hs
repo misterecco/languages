49efb6f287811a41098ed806c1b6f6765bdb151e
Tomasz Kępa <tomek@qed.ai>
2017-03-24 23:03:08 +0100

    Update - monads

--- haskell/learnyouahaskell/functors/appl.hs
+++ haskell/learnyouahaskell/functors/appl.hs
@@ -1,6 +1,5 @@
 import Control.Applicative
 
-
 main = do
   a <- myAction
   putStrLn $ "The two lines concatenated turn out to be: " ++ a
@@ -8,3 +7,13 @@ main = do
 
 myAction :: IO String
 myAction = (++) <$> getLine <*> getLine
+
+-- sequenceA :: (Applicative f) => [f a] -> f [a]
+-- sequenceA [] = pure []
+-- sequenceA (x:xs) = (:) <$> x <*> sequenceA xs
+
+
+newtype Pair b a = Pair { getPair :: (a, b)}
+
+instance Functor (Pair c) where
+  fmap f (Pair (x, y)) = Pair (f x, y)
