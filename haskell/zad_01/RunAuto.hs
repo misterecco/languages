1c8b10d967fc068d01bbdfb545d6a7e25ba013f3
Tomasz Kepa <tomek@qed.ai>
2017-03-26 10:10:19 +0200

    RunAuto has all required functionality

--- haskell/zad_01/RunAuto.hs
+++ haskell/zad_01/RunAuto.hs
@@ -1,15 +1,18 @@
 module RunAuto where
 
+import           Auto
 import           Control.Monad
 import           Data.List          (find, nub)
 import           System.Environment
 import           Text.Read
-import           Auto
 
 
+-- TODO: try using liftM
+-- TODO: better naming, formatting and styling
 -- liftM :: (Monad m) => (a -> b) -> m a -> m b
 
 type State = Int
+
 
 newtype Alpha = Alpha Char
   deriving (Eq, Show)
@@ -24,9 +27,6 @@ instance Enum Alpha where
   succ (Alpha x) = Alpha (succ x)
   pred (Alpha x) = Alpha (pred x)
 
-allAlphas :: [Alpha]
-allAlphas = [minBound..maxBound]
-
 isAlpha :: Char -> Bool
 isAlpha = (`elem` ['A'..'Z'])
 
@@ -36,28 +36,34 @@ readAlpha x = if isAlpha x then Just (Alpha x) else Nothing
 readAlphas :: String -> Maybe [Alpha]
 readAlphas = mapM readAlpha
 
+
 readStatesCount :: String -> Maybe Int
 readStatesCount = readMaybe
 
-readState :: String -> Maybe State
-readState = readMaybe
+readState :: Int -> String -> Maybe State
+readState maxState x = do
+  s <- readMaybe x
+  let allStates = [1..maxState]
+  if s `elem` allStates then return s else Nothing
 
--- TODO: check if state is in the range
-readStates :: String -> Maybe [State]
-readStates l = mapM readState (words l)
+readStates :: Int -> String -> Maybe [State]
+readStates maxState l = mapM (readState maxState) (words l)
 
-readInitStates :: String -> Maybe [State]
-readInitStates = readMaybe
+readStatesList :: Int -> String -> Maybe [State]
+readStatesList maxState x = do
+  sl <- readMaybe x
+  let allStates = [1..maxState]
+  if all (`elem` allStates) sl then return sl else Nothing
 
-readAcceptingStates :: String -> Maybe [State]
+readAcceptingStates ::  String -> Maybe [State]
 readAcceptingStates = readMaybe
 
-readTransition :: String -> Maybe [(State, Alpha, [State])]
-readTransition ln = do
+readTransition :: Int -> String -> Maybe [(State, Alpha, [State])]
+readTransition maxState ln = do
   (s:cs:dsts) <- Just $ words ln
-  state <- readState s
+  state <- readState maxState s
   letters <- readAlphas cs
-  destinationStates <- readStates $ unwords dsts
+  destinationStates <- readStates maxState $ unwords dsts
   return [(state, letter, destinationStates) | letter <- letters]
 
 readWord :: String -> Maybe [Alpha]
@@ -68,10 +74,10 @@ runAuto :: String -> Maybe Bool
 runAuto input = do
   (nos:inSt:accSt:trans) <- splitLines input
   statesCount <- readStatesCount nos
-  initSt <- readInitStates inSt
-  acceptingSt <- readAcceptingStates accSt
+  initSt <- readStatesList statesCount inSt
+  acceptingSt <- readStatesList statesCount accSt
   let (tr, [word]) = splitAtLast trans
-  let t = concat $ mapM (concat . readTransition) tr
+  let t = concat $ mapM (concat . readTransition statesCount) tr
   let a = fromLists [1..statesCount] initSt acceptingSt t
   aWord <- readWord word
   return $ accepts a aWord
@@ -86,7 +92,6 @@ splitAtLast ls = let n = length ls in splitAt (n-1) ls
 main :: IO ()
 main = do
   (filename:_) <- getArgs
-  putStrLn $ "Reading from file named: " ++ filename
   contents <- readFile filename
   let result = runAuto contents
   case result of
