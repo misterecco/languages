f2e94e944dbb7d9b5ad36f05b5abf6c20212611d
Tomasz Kępa <tomek@qed.ai>
2017-03-29 09:49:06 +0200

    Update

--- haskell/learnyouahaskell/moreMonads/writer.hs
+++ haskell/learnyouahaskell/moreMonads/writer.hs
@@ -23,4 +23,22 @@ addDrink _ = ("beer", Sum 30)
 
 
 logNumber :: Int -> Writer [String] Int
-logNumber x = Writer (x, ["Got number: " ++ show x])
+logNumber x = writer (x, ["Got number: " ++ show x])
+
+
+multWithLog :: Writer [String] Int
+multWithLog = do
+  a <- logNumber 3
+  b <- logNumber 5
+  tell ["Gonna multiply these two"]
+  return (a * b)
+
+
+gcd' :: Int -> Int -> Writer [String] Int
+gcd' a b
+  | b == 0 = do
+    tell ["Finished with " ++ show a]
+    return a
+  | otherwise = do
+    tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+    gcd' b (a `mod` b)
