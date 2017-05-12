57b55726cabd7528a8f7f109b6d93f61efd3c5e2
Tomasz Kępa <tomek@qed.ai>
2017-05-12 05:27:41 +0200

    Make saparate module for unification

--- interpreter/src/Solver.hs
+++ interpreter/src/Solver.hs
@@ -2,23 +2,16 @@ module Solver where
 
 import Db
 import AbsProlog
+import Unifier
 
 import Debug.Trace
 
+import Control.Monad.Except
 import Data.List (nub)
 import Data.List.Split (splitOn)
 import Data.Maybe (catMaybes, fromMaybe)
-import Control.Monad.Except
 
 import qualified Data.Map as M
-
-type ExceptMaybeMonad = ExceptT String Maybe
-type Subst = Variable -> Term
-data Prooftree = Done Subst | Choice [Prooftree]
-
-
-failure :: ExceptMaybeMonad a
-failure = ExceptT Nothing
 
 
 printSubs :: [Variable] -> Subst -> [String]
@@ -43,37 +36,6 @@ filterVars :: [Term] -> [Variable]
 filterVars = foldr filterVar []
 
 
-foldTerm :: (Term -> Term) -> Term -> Term
-foldTerm f v@(Var _) = f v
-foldTerm f c@(Const _) = c
-foldTerm f (Funct name terms) = Funct name $ map (foldTerm f) terms
-foldTerm f (List l) = List $ foldList l
-  where foldList ListEmpty = ListEmpty
-        foldList (ListChar s) = ListChar s
-        foldList (ListNonEmpty le) = ListNonEmpty $ foldLE le
-        foldLE (LESingle t) = LESingle $ foldTerm f t
-        foldLE (LESeq t le) = LESeq (foldTerm f t) (foldLE le)
-        foldLE (LEHead h t) = LEHead (foldTerm f h) (foldTerm f t)
-foldTerm f (OpNegate t) = OpNegate $ foldTerm f t
-foldTerm f (OpArNeg t) = OpArNeg $ foldTerm f t
-foldTerm f (OpUnifies t1 t2) = OpUnifies (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpNotUnifies t1 t2) = OpNotUnifies (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpEqual t1 t2) = OpEqual (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpNotEqual t1 t2) = OpNotEqual (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpArIs t1 t2) = OpArIs (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpArEqual t1 t2) = OpArEqual (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpArNotEqual t1 t2) = OpArNotEqual (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpArLt t1 t2) = OpArLt (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpArGt t1 t2) = OpArGt (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpArLte t1 t2) = OpArLte (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpArGte t1 t2) = OpArGte (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpArAdd t1 t2) = OpArAdd (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpArSub t1 t2) = OpArSub (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpArMul t1 t2) = OpArMul (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpArDiv t1 t2) = OpArDiv (foldTerm f t1) (foldTerm f t2)
-foldTerm f (OpArMod t1 t2) = OpArMod (foldTerm f t1) (foldTerm f t2)
-
-
 renameVar :: Int -> Term -> Term
 renameVar n (Var (Variable var)) = Var $ Variable $ name ++ "@" ++ show n
   where (name:_) = splitOn "@" var
@@ -89,14 +51,14 @@ renameClause n (Rule t gs) = Rule (renameVars n t) (map (renameVars n) gs)
 renameClause n (UnitClause t) = UnitClause $ renameVars n t
 
 
-clausesFor :: FunctorSig -> Database -> IO [Clause]
+clausesFor :: FunctorSig -> Database -> ExceptMonad [Clause]
 clausesFor fsig@(name, arity) db =
   case M.lookup fsig db of
     Just clauses -> return clauses
     Nothing -> fail $ "Undefined procedure: " ++ show name ++ "/" ++ show arity
 
 
-renameClauses :: Int -> Database -> Term -> IO [Clause]
+renameClauses :: Int -> Database -> Term -> ExceptMonad [Clause]
 renameClauses n db (Const (Atom name)) = do
   clauses <- clausesFor (name, 0) db
   return $ map (renameClause n) clauses
@@ -115,94 +77,20 @@ toTermPairs :: [Clause] -> [(Term, [Term])]
 toTermPairs = map toTermPair
 
 
-(->>) :: Variable -> Term -> Subst
-(->>) var t v | v == var = t
-              | otherwise = Var v
-
-
-(@@) :: Subst -> Subst -> Subst
-s1 @@ s2 = apply s1 . s2
-
-
-nullSubst :: Subst
-nullSubst = Var
-
-
-applyToVar :: Subst -> Term -> Term
-applyToVar s (Var v@(Variable _)) = s v
-applyToVar s t = t
-
-
-apply :: Subst -> Term -> Term
-apply s = foldTerm (applyToVar s)
-
-
-mapApply :: Subst -> [Term] -> [Term]
-mapApply = map . apply
-
-
-baseSubst :: [Subst]
-baseSubst = [nullSubst]
-
--- TODO: signal errors
-unify :: Term -> Term -> ExceptMaybeMonad [Subst]
-unify (Var x) (Var y) = if x == y then return baseSubst else return [x ->> Var y]
--- TODO: true only for Func, List, Const
-unify (Var x) t = return [x ->> t]
-unify t v@(Var x) = unify v t
-unify (Funct name1 terms1) (Funct name2 terms2) =
-  if name1 == name2 then listUnify terms1 terms2 else failure
-unify (List l1) (List l2) = unifyLists l1 l2
-unify (Const x) (Const y) = if x == y then return baseSubst else failure
-unify _ _ = failure
--- TODO: unify other types of terms
-
-unifyLists :: Lst -> Lst -> ExceptMaybeMonad [Subst]
-unifyLists ListEmpty ListEmpty = return baseSubst
-unifyLists (ListChar s1) (ListChar s2) =
-  if s1 == s2 then return baseSubst else failure
-unifyLists (ListChar s) ListEmpty = if null s then return baseSubst else failure
-unifyLists ListEmpty (ListChar s) = unifyLists (ListChar s) ListEmpty
-unifyLists (ListChar s) le@(ListNonEmpty _) = if null s then failure
-  else unifyLists (ListNonEmpty $ LEHead (List $ ListChar [head s]) (List $ ListChar $ tail s)) le
-unifyLists le@(ListNonEmpty _) lc@(ListChar _) = unifyLists lc le
-unifyLists (ListNonEmpty le1) (ListNonEmpty le2) = unifyLE le1 le2 where
-  unifyLE (LESingle t1) (LESingle t2) = unify t1 t2
-  unifyLE (LESeq t1 le1) (LESeq t2 le2) =
-    listUnify [t1, List $ ListNonEmpty le1] [t2, List $ ListNonEmpty le2]
-  unifyLE (LEHead h1 t1) (LEHead h2 t2) = listUnify [t1, h1] [t2, h2]
-  unifyLE (LESingle t1) (LEHead h t) = listUnify [t1, List ListEmpty] [h, t]
-  unifyLE le1@(LEHead _ _) le2@(LESingle _) = unifyLE le2 le1
-  unifyLE (LESeq t1 t2) (LEHead h t) = listUnify [t1, List $ ListNonEmpty t2] [h, t]
-  unifyLE leh@(LEHead _ _) les@(LESeq _ _) = unifyLE les leh
-  unifyLE _ _ = failure
-unifyLists _ _ = failure
-
-
-
-listUnify :: [Term] -> [Term] -> ExceptMaybeMonad [Subst]
-listUnify [] [] = return [nullSubst]
-listUnify (t:ts) (r:rs) = do
-  u <- unify t r
-  return [u2 @@ u1 | u1 <- u,
-                     let uu = concat $ listUnify (mapApply u1 ts) (mapApply u1 rs),
-                     u2 <- uu]
-listUnify _ _ = failure
-
-
-prooftree :: Database -> Int -> Subst -> [Term] -> IO Prooftree
+prooftree :: Database -> Int -> Subst -> [Term] -> ExceptMonad Prooftree
 prooftree db = pt where
-  pt :: Int -> Subst -> [Term] -> IO Prooftree
+  pt :: Int -> Subst -> [Term] -> ExceptMonad Prooftree
   pt _ s [] = return $ Done s
   pt n s (g:gs) = do
     renamedClauses <- renameClauses n db g
     result <- sequence [ pt (n+1) (u@@s) (mapApply u (tp++gs)) |
                        (tm, tp) <- toTermPairs (traceShowId renamedClauses),
+                       -- TODO: catch errors instead of converting to empty list
                        u <- either (const []) id (fromMaybe (Right []) (runExceptT $ unify g tm)) ]
     return $ Choice result
 
 
-search :: Prooftree -> IO [Subst]
+search :: Prooftree -> ExceptMonad [Subst]
 search (Done s) = return [s]
 search (Choice pts) = do
   let spt = map search pts
@@ -210,20 +98,29 @@ search (Choice pts) = do
   return $ concat sst
 
 
-prove :: Database -> [Term] -> IO [Subst]
+prove :: Database -> [Term] -> ExceptMonad [Subst]
 prove db t = do
   pt <- prooftree db 1 nullSubst (traceShowId t)
   search pt
 
 
+runProve :: Database -> Term -> IO [Subst]
+runProve db func = do
+  let subs = runExcept $ prove db [func]
+  case subs of
+    Left err -> fail err
+    Right v -> return v
+
+
 solve :: Database -> Term -> IO ()
 solve db (Const (Atom a)) = do
-  g <- clausesFor (a, 0) db
+  -- TOOD: do not fail silently
+  let g = clausesFor (a, 0) db
   -- TODO: it doesn't have to be unit clause
   putStrLn "true"
 solve db func@(Funct name terms) = do
   let vars = filterVars terms
-  subs <- prove db [func]
+  subs <- runProve db func
   let results = if null subs then [["false"]]
                              else map (printSubs vars) subs
   let printable = map unwords results
--- interpreter/src/Unifier.hs
+++ interpreter/src/Unifier.hs
@@ -1,1 +1,121 @@
 module Unifier where
+
+import AbsProlog
+
+import Control.Monad.Except
+
+type ExceptMaybeMonad = ExceptT String Maybe
+type ExceptMonad = Except String
+type Subst = Variable -> Term
+data Prooftree = Done Subst | Choice [Prooftree]
+
+
+failure :: ExceptMaybeMonad a
+failure = ExceptT Nothing
+
+
+nullSubst :: Subst
+nullSubst = Var
+
+
+baseSubst :: [Subst]
+baseSubst = [nullSubst]
+
+
+(->>) :: Variable -> Term -> Subst
+(->>) var t v | v == var = t
+              | otherwise = Var v
+
+
+(@@) :: Subst -> Subst -> Subst
+s1 @@ s2 = apply s1 . s2
+
+
+apply :: Subst -> Term -> Term
+apply s = foldTerm (applyToVar s)
+
+
+mapApply :: Subst -> [Term] -> [Term]
+mapApply = map . apply
+
+
+applyToVar :: Subst -> Term -> Term
+applyToVar s (Var v@(Variable _)) = s v
+applyToVar s t = t
+
+
+foldTerm :: (Term -> Term) -> Term -> Term
+foldTerm f v@(Var _) = f v
+foldTerm f c@(Const _) = c
+foldTerm f (Funct name terms) = Funct name $ map (foldTerm f) terms
+foldTerm f (List l) = List $ foldList l
+  where foldList ListEmpty = ListEmpty
+        foldList (ListChar s) = ListChar s
+        foldList (ListNonEmpty le) = ListNonEmpty $ foldLE le
+        foldLE (LESingle t) = LESingle $ foldTerm f t
+        foldLE (LESeq t le) = LESeq (foldTerm f t) (foldLE le)
+        foldLE (LEHead h t) = LEHead (foldTerm f h) (foldTerm f t)
+foldTerm f (OpNegate t) = OpNegate $ foldTerm f t
+foldTerm f (OpArNeg t) = OpArNeg $ foldTerm f t
+foldTerm f (OpUnifies t1 t2) = OpUnifies (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpNotUnifies t1 t2) = OpNotUnifies (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpEqual t1 t2) = OpEqual (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpNotEqual t1 t2) = OpNotEqual (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpArIs t1 t2) = OpArIs (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpArEqual t1 t2) = OpArEqual (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpArNotEqual t1 t2) = OpArNotEqual (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpArLt t1 t2) = OpArLt (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpArGt t1 t2) = OpArGt (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpArLte t1 t2) = OpArLte (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpArGte t1 t2) = OpArGte (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpArAdd t1 t2) = OpArAdd (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpArSub t1 t2) = OpArSub (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpArMul t1 t2) = OpArMul (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpArDiv t1 t2) = OpArDiv (foldTerm f t1) (foldTerm f t2)
+foldTerm f (OpArMod t1 t2) = OpArMod (foldTerm f t1) (foldTerm f t2)
+
+
+-- TODO: signal errors
+unify :: Term -> Term -> ExceptMaybeMonad [Subst]
+unify (Var x) (Var y) = if x == y then return baseSubst else return [x ->> Var y]
+-- TODO: true only for Func, List, Const
+unify (Var x) t = return [x ->> t]
+unify t v@(Var x) = unify v t
+unify (Funct name1 terms1) (Funct name2 terms2) =
+  if name1 == name2 then listUnify terms1 terms2 else failure
+unify (List l1) (List l2) = unifyLists l1 l2
+unify (Const x) (Const y) = if x == y then return baseSubst else failure
+unify _ _ = failure
+-- TODO: unify other types of terms
+
+
+unifyLists :: Lst -> Lst -> ExceptMaybeMonad [Subst]
+unifyLists ListEmpty ListEmpty = return baseSubst
+unifyLists (ListChar s1) (ListChar s2) =
+  if s1 == s2 then return baseSubst else failure
+unifyLists (ListChar s) ListEmpty = if null s then return baseSubst else failure
+unifyLists ListEmpty (ListChar s) = unifyLists (ListChar s) ListEmpty
+unifyLists (ListChar s) le@(ListNonEmpty _) = if null s then failure
+  else unifyLists (ListNonEmpty $ LEHead (List $ ListChar [head s]) (List $ ListChar $ tail s)) le
+unifyLists le@(ListNonEmpty _) lc@(ListChar _) = unifyLists lc le
+unifyLists (ListNonEmpty le1) (ListNonEmpty le2) = unifyLE le1 le2 where
+  unifyLE (LESingle t1) (LESingle t2) = unify t1 t2
+  unifyLE (LESeq t1 le1) (LESeq t2 le2) =
+    listUnify [t1, List $ ListNonEmpty le1] [t2, List $ ListNonEmpty le2]
+  unifyLE (LEHead h1 t1) (LEHead h2 t2) = listUnify [t1, h1] [t2, h2]
+  unifyLE (LESingle t1) (LEHead h t) = listUnify [t1, List ListEmpty] [h, t]
+  unifyLE le1@(LEHead _ _) le2@(LESingle _) = unifyLE le2 le1
+  unifyLE (LESeq t1 t2) (LEHead h t) = listUnify [t1, List $ ListNonEmpty t2] [h, t]
+  unifyLE leh@(LEHead _ _) les@(LESeq _ _) = unifyLE les leh
+  unifyLE _ _ = failure
+unifyLists _ _ = failure
+
+
+listUnify :: [Term] -> [Term] -> ExceptMaybeMonad [Subst]
+listUnify [] [] = return [nullSubst]
+listUnify (t:ts) (r:rs) = do
+  u <- unify t r
+  return [u2 @@ u1 | u1 <- u,
+                     let uu = concat $ listUnify (mapApply u1 ts) (mapApply u1 rs),
+                     u2 <- uu]
+listUnify _ _ = failure
