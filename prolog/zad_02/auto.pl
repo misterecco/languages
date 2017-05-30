f173f80db160daff80749e50caca4936de9de83b
Tomasz Kępa <tomek@qed.ai>
2017-05-30 14:11:07 +0200

    Task #2: some thiniking about the solution

--- prolog/zad_02/auto.pl
+++ prolog/zad_02/auto.pl
@@ -1,4 +1,11 @@
 /*
+ * Tomasz Kępa <tk359746@students.mimuw.edu.pl>
+ * Zadanie 2 JPP - prolog
+ */
+
+% :- use_module(library(lists)).
+
+/*
  * createLR(+Gramatyka, -Automat, -Info)
  *
  * Dla podanej gramatyki tworzy automat LR(0). W przypadku utworzenia
@@ -9,8 +16,29 @@
  * null.
  */
 
-% createLR(_, automat, yes).
-createLR(_, null, konflikt("Not implemented")).
+% gramatyka(SymbolPoczatkowy, ZbiorProdukcji).
+% prod(NazwaNieterminala, ListaPrawychStronProdukcji)
+% nt(Nieterminal)
+
+% expandGrammar(Gramatyka, RozszerzonaListaProdukcji)
+% createSituationSets(ListaProdukcji, ZbiorySytuacjiLR)
+% createAutomaton(ZbiorySytuacjiLR, Automat, Info)
+% createSituationSet(ListaProdukcji, PoczątkoweSytuacje, ZbiórSytuacjiLR)
+
+% transition(Symbol, NumerSytuacji)
+% situationSet(Produkcje, Przejścia)
+% automat(ListaProdukcji, TabelaActionGoTo)
+
+createLR(Grammar, Automat, Info) :- expandGrammar(Grammar, ProductionList),
+                                    createSituationSets(ProductionList, SituationSets),
+                                    createAutomaton(SituationSets, Automat, Info).
+
+expandGrammar(gramatyka(SP, ZP), [prod(nt(`Z`), [ [SP, `#`] ]) | ZP]).
+
+createSituationSets([ prod(SP, []) | Tail ], Automat, Info) :- createSituations(Tail, Automat, Info).
+
+expandSituationSet()
+
 
 /*
  * accept(+Automat, +Słowo)
